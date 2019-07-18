85d98ad2bd1b5bf36014086606645ee11752dc53
HAEUN JEONG <jhe3263@gmail.com>
2019-07-18 08:50:49 -0400

    Using for loop to print multiple of three

--- forLoop_multipleOfThree/Program.cs
+++ forLoop_multipleOfThree/Program.cs
@@ -10,6 +10,12 @@ namespace forLoop_multipleOfThree
     {
         static void Main(string[] args)
         {
+            int num = 3;
+            for (int i = 0; i < 10; i++)
+            {
+                //Console.WriteLine($"{num} X {i} = {num * i}");
+                Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+            }
         }
     }
 }
